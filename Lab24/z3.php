7975c2cd7eaa4c11d79f3a46e6b352d88dcf1207
ArseniySafonov <arseny060906@mail.ru>
2026-03-03 14:09:41 +0300

    Add files via upload

---
+++ Lab24/z3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+    $tit = "ССО";
+    $CSS = "a {text-decoration: none;}
+    td {width:20%;}"
+    $charset = "UTF-8";
+    echo "<meta charset = '$charset'><title> $tit </title>";
+    ?>
+</head>
+<body>
+    <?php
+    $big_font = "h3";
+     echo "<$big_font> пример PHP и html </$big_font>";
+    ?>
+</body>
+</html>
